fb9568a39680235df1579df8a06ab16ea757a229
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-29 17:05:49 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
+import React from "react";
+import Button from "./Button";
+import Alldata from "./Data";
+import Form from "./Form";
 
-import React from 'react'
-import Button  from './Button'
-import Alldata from './Data'
-
-function App()  {
+function App() {
   return (
     <>
-    <div class="main">
-    <h1>Sign up with:</h1>
-    <p><small>W3docs provides free learning materials for programming languages like HTML, CSS, Java Script, PHP etc.</small>
-    </p>
- <div className='allsite'>
- {
-   
-   Alldata.map((e, index)=> {
-     return(
-       <Button key={index} name={e.name} logo_src ={e.logo_src} bg_color={e.bg_color} />
-     )
-   }
-   )
- }
- </div>
-</div>
-  </>
-
-  )
+      <div class="main">
+        <h1>Sign up with:</h1>
+        <p>
+            W3docs provides free learning materials for programming languages
+            like HTML, CSS, Java Script, PHP etc.
+      
+        </p>
+        <div className="allsite">
+          {Alldata.map((e, index) => {
+            return (
+              <Button
+                key={index}
+                name={e.name}
+                logo_src={e.logo_src}
+                bg_color={e.bg_color}
+              />
+            );
+          })}
+        </div>
+        <Form />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
