5f7e84dc2c8f44182080da287b82d6de548447e8
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-10-02 11:19:36 +0545

    adding new functionality

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
-// import React from "react";
-// import Button from "./Button";
-// import Alldata from "./Data";
-// import Form from "./Form";
-
-// function App() {
-//   return (
-//     <>
-//       <div class="main">
-//         <h1>Sign up with:</h1>
-//         <p>
-//             W3docs provides free learning materials for programming languages
-//             like HTML, CSS, Java Script, PHP etc.
-      
-//         </p>
-//         <div className="allsite">
-//           {Alldata.map((e, index) => {
-//             return (
-//               <Button
-//                 key={index}
-//                 name={e.name}
-//                 logo_src={e.logo_src}
-//                 bg_color={e.bg_color}
-//               />
-//             );
-//           })}
-//         </div>
-//         <Form />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react';
-import SocialMediaButtons from './SocialMediaButtons';
-
+import React from "react";
+import Button from "./Button";
 function App() {
   return (
     <>
-    <div className="main">
-    <h1 className='header'>Sign Up Page </h1>
-    <p>This is a sign up page that contain three buttons<br></br> for Facebook, Twitter & Google.</p>
-      <SocialMediaButtons />
-    </div>
+      <div class="main">
+        <h1>Sign up with:</h1>
+        <p>
+          W3docs provides free learning materials for <br></br>
+          programming languages like<br></br>
+          HTML, CSS, Java Script, PHP etc.
+        </p>
+       <Button /> 
+      </div>
     </>
   );
 }
 
 export default App;
-
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import React from "react";
 
-const Form = ({ fname, email, pass }) => {
+const Form = ({media_type}) => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  alert(`${name} logged in to ${media_type}`);
+  setName('');
+  setPassword('');
+}
+
   return (
     <>
-      <div className="formInput">
-        <input placeholder="Full Name" name={fname}></input>
-        <input placeholder="abc@gmail.com" email={email}></input>
-        <input placeholder="*******" password={pass}></input>
-      </div>
+    <div className="form-class">
+    
+    <form onSubmit={handleSubmit}>
+    <h2>Login successfull to {media_type}</h2>
+    <label>
+    Enter your name:
+    <input type="text" value = {name}
+    onChange={(e)=>setName(e.target.value)}
+    />
+  </label> 
+  <br></br> 
+  <label>
+  Enter your password:
+  <input type="password" value = {password}
+  onChange={(e)=>setPassword(e.target.value)}
+  />
+</label> 
+<br></br>
+<button type="submit">Submit</button>
+    </form>
+    </div>
     </>
   );
 };
